ac9fc3d26fc534f7d61ad9473dfeae00693f5672
Holly Wilkalis <holly.wilkalis@gmail.com>
2017-10-24 21:50:59 -0700

    Modify leapYear function to check for 2nd step

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 var leapYear = function(year) {
-  if (year % 4 === 0) {
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
     return true;
   } else {
     return false;
   }
 };
+
 
 
 
